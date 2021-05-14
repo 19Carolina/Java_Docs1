bcf5492f0313c0f89d13e8e09301760e896eee76
carol <carol@LAPTOP-5JKMQ1NT>
2021-05-14 17:38:53 -0500

    EJERCICIO DE HTML

---
+++ src/java_docs1/Java_Docs1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_docs1;
+
+/**
+ *
+ * @author carolina Del Carmen Colli Aguilar 
+ * @author Universidad Autónoma de Campeche
+ * @since  14/Mayo/2021
+ * @author Segundo semestre
+ * @author Grupo B
+ * @author Ingeniería en Sistemas Computacionales
+ */
+public class Java_Docs1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Area del rectangulo
+        System.out.println("AREA DEL RECTANGULO");
+        double base = 30;
+         System.out.println("La base del rectangulo es:" + base);
+        double altura = 15;
+        System.out.println("La altura del rectangulo es:" + altura);
+
+        double area = base*altura;
+        System.out.println("El area del rectangulo es:" + area);
+    }
+    
+}
